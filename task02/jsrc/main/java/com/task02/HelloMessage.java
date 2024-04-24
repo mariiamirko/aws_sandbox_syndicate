f62c68b59f86c30e5fa8c2617e2b10aeada3d34e
mariia.mirko <mariia.mirko@gmail.com>
2024-04-24 23:05:31 +0200

    task02

--- task02/jsrc/main/java/com/task02/HelloMessage.java
+++ task02/jsrc/main/java/com/task02/HelloMessage.java
@@ -1,19 +1,10 @@
-/*
- * Copyright 2024 American Well Systems.
- * All rights reserved.
- *
- * It is illegal to use, reproduce or distribute
- * any part of this Intellectual Property without
- * prior written authorization from American Well.
- */
-
 package com.task02;
 
 public class HelloMessage {
 	private String message;
-	private int status;
-	public HelloMessage(String message, int status){
+	private int statusCode;
+	public HelloMessage(String message, int statusCode){
 		this.message = message;
-		this.status = status;
+		this.statusCode = statusCode;
 	}
 }
