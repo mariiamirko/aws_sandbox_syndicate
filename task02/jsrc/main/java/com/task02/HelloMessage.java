571c98f94b926536ec00e4af815b731c5baba388
mariia.mirko <mariia.mirko@gmail.com>
2024-04-24 22:59:02 +0200

    task02

---
+++ task02/jsrc/main/java/com/task02/HelloMessage.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2024 American Well Systems.
+ * All rights reserved.
+ *
+ * It is illegal to use, reproduce or distribute
+ * any part of this Intellectual Property without
+ * prior written authorization from American Well.
+ */
+
+package com.task02;
+
+public class HelloMessage {
+	private String message;
+	private int status;
+	public HelloMessage(String message, int status){
+		this.message = message;
+		this.status = status;
+	}
+}
---
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+@LambdaHandler(lambdaName = "hello_world",
+		roleName = "hello_world-role",
+		isPublishVersion = true,
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+	private static final Gson gson = new Gson();
+
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent,
+			Context context) {
+		return new APIGatewayProxyResponseEvent()
+				.withStatusCode(200)
+				.withBody(gson.toJson(new HelloMessage("Hello from Lambda", 200)));
+	}
+}
