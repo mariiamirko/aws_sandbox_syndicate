f1f5b8d8b3568ab77566dc79257b6556b70550e6
mariia.mirko <mariia.mirko@gmail.com>
2024-05-03 23:37:04 +0200

    task04

---
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -0,0 +1,28 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "sqs_handler",
+	roleName = "sqs_handler-role",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SqsTriggerEventSource(targetQueue = "async_queue", batchSize = 100)
+public class SqsHandler implements RequestHandler<SQSEvent, Void > {
+
+	public Void handleRequest(SQSEvent sqsEvent, Context context) {
+		LambdaLogger logger = context.getLogger();
+		for(SQSEvent.SQSMessage message: sqsEvent.getRecords()){
+			logger.log(message.getBody());
+		}
+		return null;
+	}
+}
