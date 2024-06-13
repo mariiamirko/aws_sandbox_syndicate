a8eeb9eb2bd1fd53e7ad816074f1e8f0c4af08f5
mariia.mirko <mariia.mirko@gmail.com>
2024-06-13 21:37:14 +0200

    task02 use APIGatewayV2HTTPEvent

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -2,8 +2,8 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.google.gson.Gson;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
@@ -19,13 +19,16 @@ import com.syndicate.deployment.model.lambda.url.InvokeMode;
 		authType = AuthType.NONE,
 		invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayProxyResponseEvent> {
 	private static final Gson gson = new Gson();
 
 	@Override
-	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent,
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayV2HTTPEvent apiGatewayProxyRequestEvent,
 			Context context) {
-		if (apiGatewayProxyRequestEvent.getPath().equals("/hello")) {
+		String method = apiGatewayProxyRequestEvent.getRequestContext().getHttp().getMethod();
+		String path = apiGatewayProxyRequestEvent.getRequestContext().getHttp().getPath();
+
+		if (path.equals("/hello")) {
 			return new APIGatewayProxyResponseEvent()
 					.withStatusCode(200)
 					.withBody(gson.toJson(new HelloMessage("Hello from Lambda", 200)));
@@ -34,6 +37,6 @@ public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, A
 			return new APIGatewayProxyResponseEvent()
 					.withStatusCode(400)
 					.withBody(String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s",
-							apiGatewayProxyRequestEvent.getPath(), apiGatewayProxyRequestEvent.getHttpMethod()));
+							path, method));
 	}
 }
